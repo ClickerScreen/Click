2e288ab1a50dbe071e26c9843623186ca5be6f7c
Jmumford45 <JMumford45@gmail.com>
2021-05-02 03:57:13 -0700

    testActivity tests populating given number of buttons at random positions with uniquie ids and now can populate at interval, cancel condition in create buttons function, override ondestroy to make sure timer stops when leaving app

--- app/src/main/java/com/example/clickinggameproject/testButtonsActivity.kt
+++ app/src/main/java/com/example/clickinggameproject/testButtonsActivity.kt
@@ -1,25 +1,33 @@
 package com.example.clickinggameproject
 
+import android.app.job.JobScheduler
 import android.graphics.drawable.Drawable
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Contacts
 import android.util.DisplayMetrics
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.annotation.RestrictTo
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
 import java.util.*
 import kotlin.concurrent.timerTask
 import kotlin.random.Random
+import kotlin.coroutines.*
 
 class testButtonsActivity : AppCompatActivity() {
     private var numOfBtns: Int = 0
     private var randX = 0
     private var randY = 0
-    private val numberOfButtonsToCreate = 3
+    private val numberOfButtonsToCreate = 4
     private val timer: Timer = Timer()
+    private var delayTime: Long = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +41,20 @@ class testButtonsActivity : AppCompatActivity() {
         CreateButtonTimer(width, height)
     }
 
+    val scope = MainScope()
+    fun NotClicked(button: Button) = scope.launch {
+        delay(delayTime)
+        button.isVisible = false
+    }
+
+    val localScope = CoroutineScope(Dispatchers.Default)
+    fun ButtonNotClicked(button: Button) = localScope.launch {
+        delay(3000)
+        button.isVisible = false
+    }
+
     fun CreateButtonTimer(screenX: Int, screenY: Int) {
-        var timerTask = timerTask {
+        val timerTask = timerTask {
             run() {
                 randX = Random.nextInt(30, screenX - 250)
                 randY = Random.nextInt(90, screenY - 144)
@@ -42,6 +62,15 @@ class testButtonsActivity : AppCompatActivity() {
             }
         }
         timer.schedule(timerTask, 250, 1000)
+    }
+
+    fun MoveButton(_createdButton: Button) {
+        randX = Random.nextInt(30, 1080 - 250)
+        randY = Random.nextInt(90, 1794 - 144)
+
+        // change the position of the button
+        _createdButton.x = randX.toFloat()
+        _createdButton.y = randY.toFloat()
     }
 
     fun CreateButtons(posx: Int, posy: Int) {
@@ -61,16 +90,24 @@ class testButtonsActivity : AppCompatActivity() {
             addContentView(btn, params)
 
             val _createdButton = findViewById<Button>(btn.id)
+            var job = NotClicked(_createdButton)
+
             _createdButton.setOnClickListener()
             {
-                Toast.makeText(this, "Button clicked index = " + btn.id, Toast.LENGTH_SHORT).show()
-            }
+                job.cancel()
 
+                if(job.isCancelled) {
+                    job = NotClicked(_createdButton)
+                }
+
+                MoveButton(_createdButton)
+            }
             if (numOfBtns == numberOfButtonsToCreate) {
                 timer.cancel()
             }
         }
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
