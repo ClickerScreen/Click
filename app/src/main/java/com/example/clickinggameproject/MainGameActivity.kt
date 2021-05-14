5051a09c37a6f76477cef6fd1b95ddc742ce94cf
Jmumford45 <JMumford45@gmail.com>
2021-05-14 15:27:13 -0700

    added background changing gradient and button sound. button sound isn't perfect, need to see if it continues to use resources, sounds don't play on multiple clicks before sound completes

--- app/src/main/java/com/example/clickinggameproject/MainGameActivity.kt
+++ app/src/main/java/com/example/clickinggameproject/MainGameActivity.kt
@@ -1,24 +1,46 @@
 package com.example.clickinggameproject
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
 import android.util.DisplayMetrics
-import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
+import kotlinx.coroutines.*
+import java.util.*
+import kotlin.concurrent.timerTask
 import kotlin.random.Random
 
 
 class MainGameActivity : AppCompatActivity() {
-    var counter: Int = 10
+    var counter: Int = 20
+    private var numOfBtns: Int = 0
+    private var randX = 0
+    private var randY = 0
+    private val numberOfButtonsToCreate = 8
+    private val timer: Timer = Timer()
+    private var delayTime: Long = 2000
+    var points = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_game)
+
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.layout)
+        val animationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2000)
+        animationDrawable.setExitFadeDuration(4000)
+        animationDrawable.start()
+
+        val buttonSound: MediaPlayer = MediaPlayer.create(this, R.raw.coin_collect)
 
         val displaymetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displaymetrics)
@@ -26,13 +48,9 @@ class MainGameActivity : AppCompatActivity() {
         val height = displaymetrics.heightPixels // 1794
         var score = 0
 
-        var soundButton = findViewById<ImageButton>(R.id.buttonSound)
-        var randX = 0
-        var randY = 0
-
         val countDown = findViewById<TextView>(R.id.countDown)
 
-        var timer = object : CountDownTimer(10000, 1000)
+        var timer = object : CountDownTimer(20000, 1000)
         {
             override fun onFinish() {
                 countDown.text = "Finished"
@@ -43,6 +61,8 @@ class MainGameActivity : AppCompatActivity() {
                     intent.putExtra("currentScore", score)
                     startActivity(intent)
                 }, 1000)
+
+                buttonSound.release()
             }
 
             override fun onTick(millisUnitlFinished: Long)
@@ -53,22 +73,9 @@ class MainGameActivity : AppCompatActivity() {
         }.start()
         
         var gameScore = findViewById<TextView>(R.id.textViewGameScore)
-        gameScore.setText("Score = " + score)
+        gameScore.setText("Score = " + points)
 
-        // change button position on button click
-        soundButton.setOnClickListener{
-            // add to the score visualizer on the screen
-            score += 1
-            gameScore.setText("Score = " + score)
-
-            // generate new position
-            randX = Random.nextInt(30, width - 250)
-            randY = Random.nextInt(90, height - 144)
-
-            // change the position of the button
-            soundButton.x = randX.toFloat()
-            soundButton.y = randY.toFloat()
-        }
+        CreateButtonTimer(width, height, gameScore, buttonSound)
 
         //commenting out for now since leaderboard activity can send back to main menu now
         //also set the button to invisible it can be removed when we are happy with how it functions
@@ -77,5 +84,82 @@ class MainGameActivity : AppCompatActivity() {
             //val intent = Intent(this, LeaderBoardActivity::class.java)
             //startActivity(intent)
         //}
+    }
+
+    val scope = MainScope()
+    fun NotClicked(button: Button) = scope.launch {
+        delay(delayTime)
+        button.isVisible = false
+    }
+
+    val localScope = CoroutineScope(Dispatchers.Default)
+    fun ButtonNotClicked(button: Button) = localScope.launch {
+        delay(3000)
+        button.isVisible = false
+    }
+
+    fun CreateButtonTimer(screenX: Int, screenY: Int, score: TextView, sound: MediaPlayer) {
+        val timerTask = timerTask {
+            run() {
+                randX = Random.nextInt(30, screenX - 250)
+                randY = Random.nextInt(90, screenY - 144)
+                CreateButtons(randX, randY, score, sound)
+            }
+        }
+        timer.schedule(timerTask, 250, 1000)
+    }
+
+    fun MoveButton(_createdButton: Button) {
+        randX = Random.nextInt(30, 1080 - 250)
+        randY = Random.nextInt(90, 1794 - 144)
+
+        // change the position of the button
+        _createdButton.x = randX.toFloat()
+        _createdButton.y = randY.toFloat()
+    }
+
+    fun CreateButtons(posx: Int, posy: Int, score:TextView, sound: MediaPlayer) {
+        this.runOnUiThread() {
+            val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            val btn = Button(this)
+            btn.id = numOfBtns
+            numOfBtns++
+            btn.setBackgroundResource(R.drawable.coin)
+
+            btn.x = posx.toFloat()
+            btn.y = posy.toFloat()
+
+            addContentView(btn, params)
+
+            val _createdButton = findViewById<Button>(btn.id)
+            var job = NotClicked(_createdButton)
+
+            _createdButton.setOnClickListener()
+            {
+                job.cancel()
+                sound.start()
+
+                if(job.isCancelled)
+                    job = NotClicked(_createdButton)
+
+                // add to the score visualizer on the screen
+                points  += 1
+                score.setText("Score = " + points)
+
+                MoveButton(_createdButton)
+            }
+            if (numOfBtns == numberOfButtonsToCreate) {
+                timer.cancel()
+            }
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
     }
 }
